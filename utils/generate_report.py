793cccfa849d00160cb9b5c4294eb028d774c361
raunakm90 <raunakm90@gmail.com>
2017-08-21 17:22:08 -0500

    Functions to generate reports

--- utils/generate_report.py
+++ utils/generate_report.py
@@ -11,49 +11,6 @@ matplotlib.rc('font', **font)
 sns.set(font_scale=1.5)
 
 
-def flat_list_of_array(l):
-    return np.concatenate(l).ravel()
-
-
-# @TODO: Add mask to hide 0 entries in confusion matrix
-def plot_confusion_matrix(cm, labels, title_text='Confusion Matrix', file_name="Confusion_Matrix.png"):
-    plt.figure(figsize=(25, 8))
-    acc = np.sum(cm.diagonal()) / np.sum(cm)
-    cm_2 = cm / np.sum(cm, axis=1)[:, np.newaxis]
-    cm_2 = np.append(cm_2, np.sum(cm, axis=1).reshape(len(labels), 1), axis=1)
-    x_labels = np.append(labels, 'Support')
-    sns.heatmap(cm_2, annot=True, fmt='.2f', vmin=0, vmax=1, xticklabels=x_labels, yticklabels=labels)
-    plt.title(title_text, fontsize=20)
-    plt.xlabel('Predicted', fontsize=15)
-    plt.ylabel('Actual Class', fontsize=15)
-    plt.yticks(rotation=0)
-    plt.savefig(file_name)
-
-
-def plot_classification_report(report, file_name):
-    plt.figure(figsize=(25, 8))
-    sns.heatmap(report, annot=True, fmt='.2f', vmin=0, vmax=1)
-    plt.title("Classification Report", fontsize=20)
-    plt.xlabel('Metric', fontsize=15)
-    plt.ylabel('Gesture Class', fontsize=15)
-    plt.yticks(rotation=0)
-    plt.savefig(file_name)
-
-
-def classification_report(y_true, y_pred, target_names, file_path):
-    precision, recall, f1, support = mt.precision_recall_fscore_support(y_true, y_pred)
-    conf_mat = mt.confusion_matrix(y_true=y_true, y_pred=y_pred)
-    per_class_acc = conf_mat.diagonal() / np.sum(conf_mat, axis=1)
-
-    report_df = pd.DataFrame([precision, recall, f1, per_class_acc, support], columns=target_names).T
-    report_df.columns = ['Precision', 'Recall', 'F1_Score', 'Accuracy', 'Support']
-
-    plot_confusion_matrix(conf_mat, target_names, file_name=file_path + "Confusion_Matrix.png")
-    plot_classification_report(report_df, file_name=file_path + "Classification_Report.png")
-
-    return report_df
-
-
 def write_results(train_sizes, train_scores, test_scores, file_path):
     print("Writing results...")
     np.savez(file_path + "_Train_Sizes.npz", train_sizes)
@@ -62,39 +19,103 @@ def write_results(train_sizes, train_scores, test_scores, file_path):
 
 
 # Training history for DL models
-def plot_train_hist(train_val_hist, file_path):
-    # Plot cross entropy loss
-    fig, axarr = plt.subplots(1, 2, sharey=True, figsize=(18, 8))
-    i = 1
-    for item in train_val_hist:
-        axarr[0].plot(item.history['loss'], label='User_' + str(i))
-        axarr[0].set_xlabel("Number of Epochs")
-        axarr[0].set_ylabel("Loss")
-        axarr[1].plot(item.history['val_loss'], label='User_' + str(i))
-        axarr[1].set_xlabel("Number of Epochs")
-        axarr[1].set_ylabel("Loss")
-        i += 1
-    axarr[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.suptitle("Cross-Entropy Loss History")
-    axarr[0].set_title("Training Cross-Entropy Loss")
-    axarr[1].set_title("Validation Cross-Entropy Loss")
-    plt.savefig(file_path + "Loss_History.png")
+def plot_training_curve(file_path):
+    train_loss = np.load(file_path + "_Train_Loss.npz")['arr_0']
+    val_loss = np.load(file_path + "_Val_Loss.npz")['arr_0']
 
-    # Plot accuracy
-    fig, axarr = plt.subplots(1, 2, sharey=True, figsize=(18, 8))
-    i = 1
-    for item in train_val_hist:
-        axarr[0].plot(item.history['acc'], label='User_' + str(i))
-        axarr[0].set_xlabel("Number of Epochs")
-        axarr[0].set_ylabel("Accuracy")
-        axarr[1].plot(item.history['val_acc'], label='User_' + str(i))
-        axarr[1].set_xlabel("Number of Epochs")
-        axarr[1].set_ylabel("Accuracy")
-        i += 1
-    axarr[1].legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.suptitle("Accuracy History")
-    axarr[0].set_title("Training Accuracy")
-    axarr[1].set_title("Validation Accuracy")
-    plt.savefig(file_path + "Accuracy_History.png")
+    train_acc = np.load(file_path + "_Train_Acc.npz")['arr_0']
+    val_acc = np.load(file_path + "_Val_Acc.npz")['arr_0']
 
-    return None
+    # Average and SD for loss across users
+    train_loss_avg = np.mean(train_loss, axis=0)
+    val_loss_avg = np.mean(val_loss, axis=0)
+    train_loss_std = np.std(train_loss, axis=0)
+    val_loss_std = np.std(val_loss, axis=0)
+
+    # Average and Loss for accuracy across users
+    train_acc_avg = np.mean(train_acc, axis=0)
+    val_acc_avg = np.mean(val_acc, axis=0)
+    train_acc_std = np.std(train_acc, axis=0)
+    val_acc_std = np.std(val_acc, axis=0)
+
+    nb_epoch = np.arange(0, len(train_loss_avg))
+
+    fig, axarr = plt.subplots(1, 2, sharey=False, figsize=(25, 8))
+    plt.suptitle("Training Performance", fontsize=20)
+
+    axarr[0].set_xlabel("Number of Iterations")
+    axarr[0].set_ylabel("Cross-Entropy Loss")
+    axarr[0].set_title("Model Performance: Cross-Entropy Loss")
+    axarr[0].fill_between(nb_epoch, train_loss_avg - train_loss_std,
+                          train_loss_avg + train_loss_std, alpha=0.1,
+                          color="r")
+    axarr[0].fill_between(nb_epoch, val_loss_avg - val_loss_std,
+                          val_loss_avg + val_loss_std, alpha=0.1, color="g")
+    axarr[0].plot(nb_epoch, train_loss_avg, color="r")
+    axarr[0].plot(nb_epoch, val_loss_avg, color="g")
+
+    axarr[1].set_xlabel("Number of Iterations")
+    axarr[1].set_ylabel("Accuracy")
+    axarr[1].set_title("Model Performance: Accuracy")
+    axarr[1].fill_between(nb_epoch, train_acc_avg - train_acc_std,
+                          train_acc_avg + train_acc_std, alpha=0.1,
+                          color="r")
+    axarr[1].fill_between(nb_epoch, val_acc_avg - val_loss_std,
+                          val_acc_avg + val_acc_std, alpha=0.1, color="g")
+    axarr[1].plot(nb_epoch, train_acc_avg, color='r')
+    axarr[1].plot(nb_epoch, val_acc_avg, color='g')
+
+    plt.savefig(file_path + "Training_Performance.png")
+
+    return train_acc, val_acc
+
+
+def plot_training_curve_baseline(train_scores, test_scores, train_sizes, subplot_titles):
+    fig, axarr = plt.subplots(1, 2, sharey=True, figsize=(25, 8))
+    plt.suptitle("Baseline Models Training Performance", fontsize=20)
+
+    for i in range(len(train_scores)):
+        axarr[i].grid()
+        axarr[i].set_xlabel("Training examples")
+        axarr[i].set_ylabel("Score")
+        train_scores_mean = np.mean(train_scores[i], axis=1)
+        train_scores_std = np.std(train_scores[i], axis=1)
+        test_scores_mean = np.mean(test_scores[i], axis=1)
+        test_scores_std = np.std(test_scores[i], axis=1)
+
+        axarr[i].fill_between(train_sizes[i], train_scores_mean - train_scores_std,
+                              train_scores_mean + train_scores_std, alpha=0.1,
+                              color="r")
+        axarr[i].fill_between(train_sizes[i], test_scores_mean - test_scores_std,
+                              test_scores_mean + test_scores_std, alpha=0.1, color="g")
+        axarr[i].plot(train_sizes[i], train_scores_mean, 'o-', color="r",
+                      label="Training Score")
+        axarr[i].plot(train_sizes[i], test_scores_mean, 'o-', color="g",
+                      label="Cross-Validation Score")
+        axarr[i].legend(loc="best")
+        axarr[i].set_title(subplot_titles[i])
+    return plt
+
+
+
+def write_results_models(train_scores, test_scores, class_names, y_pred, y_true, file_path):
+    print("Writing results...")
+    np.savez(file_path + "_Train_Scores.npz", train_scores)
+    np.savez(file_path + "_Test_Scores.npz", test_scores)
+    np.savez(file_path + "_Predictions.npz", y_pred)
+    np.savez(file_path + "_Truth.npz", y_true)
+    np.savez(file_path + "_Class_Names.npz", class_names)
+
+
+def write_train_hist(train_val_hist, file_path):
+    train_loss, val_loss = [], []
+    train_acc, val_acc = [], []
+    for item in train_val_hist:
+        train_loss.append(item.history['loss'])
+        val_loss.append(item.history['val_loss'])
+        train_acc.append(item.history['acc'])
+        val_acc.append(item.history['val_acc'])
+    np.savez(file_path + "_Train_Loss.npz", train_loss)
+    np.savez(file_path + "_Train_Acc.npz", train_acc)
+    np.savez(file_path + "_Val_Loss.npz", val_loss)
+    np.savez(file_path + "_Val_Acc.npz", val_acc)
